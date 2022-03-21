0fa57ec5ef55867a9b14f6a0fe1475c2ab6893d0
mofulbright <mofulbright02@gmail.com>
2022-03-21 13:35:27 -0300

    initial

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int addition = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"a + b is equal to {addition}");
+            Console.WriteLine($"a - b is equal to {subtraction}");
+            Console.WriteLine($"a * b is equal to {multiplication}");
+            Console.WriteLine($"a / b is equal to {quotient} remainder {remainder}");
+        }
+    }
+}
