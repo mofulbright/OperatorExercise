15f8b10c0f4e2d8d7163155820397d2b6d4571de
mofulbright <mofulbright02@gmail.com>
2022-03-22 16:14:51 -0300

    second

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -10,13 +10,22 @@
             int addition = a + b;
             int subtraction = a - b;
             int multiplication = a * b;
-            int quotient = a / b;
+            double quotient = a / b;
             int remainder = a % b;
+            Console.WriteLine($"{a} + {b} is equal to {addition}");
+            Console.WriteLine($"{a} - {b} is equal to {subtraction}");
+            Console.WriteLine($"{a} * {b} is equal to {multiplication}");
+            Console.WriteLine($"{a} / {b} is equal to {quotient} remainder {remainder}");
 
-            Console.WriteLine($"a + b is equal to {addition}");
-            Console.WriteLine($"a - b is equal to {subtraction}");
-            Console.WriteLine($"a * b is equal to {multiplication}");
-            Console.WriteLine($"a / b is equal to {quotient} remainder {remainder}");
+            Console.WriteLine(AreaOfCircle());
+
+        }
+        static double AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
         }
     }
 }
